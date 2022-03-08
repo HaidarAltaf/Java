757f09b9b5a7cffef3fb9441ea9472b8d49c8245
Haidar <haidaraltaf@gmail.com>
2022-03-08 16:08:48 +0000

    shark example not finished

--- SpringShark/src/main/java/com/qa/demo/controller/SharkController.java
+++ SpringShark/src/main/java/com/qa/demo/controller/SharkController.java
@@ -1,10 +1,13 @@
 package com.qa.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.demo.service.SharkService;
-
 
 @RestController
 @RequestMapping("/shark")
@@ -14,6 +17,14 @@ public class SharkController {
 	private SharkController(SharkService service) {
 		this.service = service;
 	}
-	
-	
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> deleteShark(@PathVariable long id) {
+		if (this.service.delete(id) == true) {
+			new ResponseEntity<Boolean>(HttpStatus.NO_CONTENT);
+		} else {
+			new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
+		}
+		return null;
+	}
 }
